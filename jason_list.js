28eb8dff6fc005785525f1e37e9785583093351d
TONY <130157657+TonySnackPe@users.noreply.github.com>
2024-07-14 11:13:51 -0500

    Add files via upload

---
+++ jason_list.js
@@ -0,0 +1,51 @@
+
+var friends = [
+    {
+        name: 'My Love ',
+        user: 'My Love ',
+        url: 'iSrCriss',
+        img: 'https://xatimg.com/image/BHUt8flQI5cs.gif'
+    },
+    {
+        name: 'SoolG',
+        user: 'SoolG',
+        url: 'xSoolG',
+        img: 'https://xatimg.com/image/BHUt8flQI5cs.gif'
+    },
+    {
+        name: 'Pitu',
+        user: 'Pitu',
+        url: 'xPiitu',
+        img: 'https://xatimg.com/image/BHUt8flQI5cs.gif'
+    },
+    {
+        name: 'Mont',
+        user: 'Mont',
+        url: 'xMont',
+        img: 'https://xatimg.com/image/BHUt8flQI5cs.gif'
+    },
+    {
+        name: 'Teki',
+        user: 'Teki',
+        url: 'Teki',
+        img: 'https://xatimg.com/image/BHUt8flQI5cs.gif'
+    },
+    {
+        name: 'Luna',
+        user: 'Luna',
+        url: 'LDuLzura25',
+        img: 'https://xatimg.com/image/BHUt8flQI5cs.gif'
+    },
+    {
+        name: 'Gaston',
+        user: 'Gaston',
+        url: 'iGaston',
+        img: 'https://xatimg.com/image/BHUt8flQI5cs.gif'
+    },
+    {
+        name: 'Jack',
+        user: 'Jack ',
+        url: 'Jackk',
+        img: 'https://xatimg.com/image/BHUt8flQI5cs.gif'
+    }
+];
